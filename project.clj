6ff2f7a75d1d47ca87cad9a0defadd6be4f42bce
Brandon Adams <emidln@gmail.com>
2015-08-07 12:13:32 -0500

    Allow the repl more ram and more time to load for slow laptops

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :jvm-optons ["-Xmx1G"]
+  :repl-options {:timeout 120000}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [prismatic/plumbing "0.4.4"]
                  [prismatic/schema "0.4.3"]
