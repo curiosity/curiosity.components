3c98f5ca9e6ff619f7bc035f08cb6d6f078ada32
Brandon Adams <emidln@gmail.com>
2015-08-07 12:14:47 -0500

    Bump version for api breakage

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject curiosity.components "0.1.0-SNAPSHOT"
+(defproject curiosity.components "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
