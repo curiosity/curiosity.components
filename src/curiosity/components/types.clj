42292c41de45b0b54263b123a4d8f9bca01bc768
Brandon Adams <emidln@gmail.com>
2015-08-10 17:15:20 -0500

    define types.clj in terms of protocols

--- src/curiosity/components/types.clj
+++ src/curiosity/components/types.clj
@@ -1,7 +1,7 @@
 (ns curiosity.components.types
   (:require [schema.core :as s]))
 
-(def Vector (class []))
-(def Map (class {}))
-(def Set (class #{}))
+(def Vector (s/pred vector?))
+(def Map (s/pred map?))
+(def Set (s/pred set?))
 (def Fn (s/pred fn?))
