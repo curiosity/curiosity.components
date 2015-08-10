9e56f2f57d1de328cbfa9ae493bb901548028621
Brandon Adams <emidln@gmail.com>
2015-08-10 14:12:36 -0500

    Added with-redis macro

--- src/curiosity/components/redis.clj
+++ src/curiosity/components/redis.clj
@@ -1,6 +1,7 @@
 (ns curiosity.components.redis
   (:require [com.stuartsierra.component :as component]
             [schema.core :as s]
+            [taoensso.carmine :as car]
             [plumbing.core :refer :all]))
 
 (s/defrecord PooledCarmine
@@ -8,6 +9,8 @@
 
   component/Lifecycle
   (start [this]
+    ;; we reuse the pool if we have one when we start
+    ;; it's with-redis's responsibility to maintain this
     (assoc this :conn-opts {:pool {}
                             :spec {:uri redis-uri}}))
   (stop [this]
@@ -16,3 +19,9 @@
 (defnk new-pooled-redis
   [redis-uri :- s/Str]
   (map->PooledCarmine {:redis-uri redis-uri}))
+
+(defmacro with-redis
+  "Given a redis component and some commands, run commands in the context of the redis component's conn"
+  {:arglists '([redis-component :as-pipeline & body] [redis-component & body])}
+  [redis-component & sigs]
+  `(car/wcar ~(:conn-opts redis-component) ~@body))
