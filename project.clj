4f0ac120b8aa1aad1cfdb6046a149f1e66893b3f
Ben Siver <bsiver@gmail.com>
2015-11-25 11:29:30 -0600

    Release v0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject curiosity.components "0.3.0-SNAPSHOT"
+(defproject curiosity.components "0.3.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
