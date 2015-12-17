5fec8cc27ebeca0bc3fffe495cb3b3b33c2bc212
Brandon Adams <emidln@gmail.com>
2015-12-17 16:08:53 -0600

    Move to 0.4.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject curiosity.components "0.3.0"
+(defproject curiosity.components "0.4.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
