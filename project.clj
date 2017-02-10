a4bede7f50610c35e9ed55858e47ef14bd2cd017
Brandon Adams <emidln@gmail.com>
2017-02-10 11:41:31 -0600

    Next dev version; improved logging

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject curiosity.components "0.6.1-alpha9"
+(defproject curiosity.components "0.7.0-SNAPSHOT"
   :description "Curiosity.com components. Proprietary. Do not distribute."
   :url "http://github.com/curiosity/curiosity.components"
   :license {:name "Proprietary. Do not distribute."
--- src/curiosity/components/logging.clj
+++ src/curiosity/components/logging.clj
@@ -1,8 +1,13 @@
 (ns curiosity.components.logging
+  "Basic logging component using timbre.
+  NOTE: You probably want to require this namespace prior to everything else."
   (:require [com.stuartsierra.component :as component]
             [schema.core :as s]
             [taoensso.timbre :as timbre]))
 
+;; setup a default error level
+(timbre/info "Setting default Timbre :level to :error.")
+(timbre/set-level! :error)
 
 (defrecord TimbreLogging
     [level config]
@@ -10,6 +15,7 @@
   (start [this]
     (do
       (timbre/set-level! (or level :info))
+      (timbre/handle-uncaught-jvm-exceptions!)
       this))
   (stop [this] this))
 
@@ -24,7 +30,4 @@
 (defn new-timbre-logging
   []
   (TimbreLogging. nil nil))
-
-
-
 
