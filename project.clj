ef28490f7b38dcea97feaf5e9df56f89b9b57e49
Ben Siver <bsiver@gmail.com>
2017-06-21 15:47:33 -0500

    0.9.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject curiosity.components "0.8.17"
+(defproject curiosity.components "0.9.0-SNAPSHOT"
   :description "Curiosity.com components. Proprietary. Do not distribute."
   :url "http://github.com/curiosity/curiosity.components"
   :license {:name "Proprietary. Do not distribute."
